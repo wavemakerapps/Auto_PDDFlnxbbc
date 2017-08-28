b2fcab1cc5c09e4135ced19617724dafac0058ea
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-28 22:18:08 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_PDDFlnxbbc",
+  "homePage" : "Main",
+  "name" : "Auto_PDDFlnxbbc",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
